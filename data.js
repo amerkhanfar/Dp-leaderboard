c6972f00bcacabf55aeb2e9acb3c031bd4dbdb23
Amer Khanfar <abk.sg8@gmail.com>
2023-02-07 13:09:48 +0400

    added dummy data

---
+++ data.js
@@ -0,0 +1,142 @@
+export const Participants = [
+  {
+    id: 1,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 2,
+    name: "amer khanfar",
+    score: 2,
+  },
+  {
+    id: 3,
+    name: "amer khanfar",
+    score: 3,
+  },
+  {
+    id: 4,
+    name: "amer khanfar",
+    score: 4,
+  },
+  {
+    id: 5,
+    name: "amer khanfar",
+    score: 5,
+  },
+  {
+    id: 6,
+    name: "amer khanfar",
+    score: 6,
+  },
+  {
+    id: 7,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 10,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 8,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 9,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 11,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 12,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 13,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 14,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 15,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 16,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 17,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 18,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 19,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 20,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 21,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 22,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 23,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 24,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 25,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 26,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 27,
+    name: "amer khanfar",
+    score: 1,
+  },
+  {
+    id: 28,
+    name: "amer khanfar",
+    score: 1,
+  },
+];
